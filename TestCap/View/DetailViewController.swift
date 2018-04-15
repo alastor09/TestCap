1d274e3c2d59e90178eb8e0f1a9515f1bafae239
Soan Saini <soan09@yahoo.com>
2018-04-15 12:25:06 +1000

    - ReImplemented Landscape and portrait requirement,

--- TestCap/View/DetailViewController.swift
+++ TestCap/View/DetailViewController.swift
@@ -11,12 +11,6 @@ import SDWebImage
 
 class DetailViewController: UIViewController {
     
-    @IBOutlet weak var backgroundView: UIView!
-    @IBOutlet weak var heightOfImage: NSLayoutConstraint!
-    @IBOutlet weak var widthOfImage: NSLayoutConstraint!
-    @IBOutlet weak var heightOfLabel: NSLayoutConstraint!
-    @IBOutlet weak var widthOfLabel: NSLayoutConstraint!
-    
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var topImageView: UIImageView!
     var viewModel : HomeCellViewModel!
@@ -32,52 +26,19 @@ class DetailViewController: UIViewController {
         if let url = viewModel.imageUrl {
             self.topImageView.sd_setImage(with: URL(string: url), placeholderImage: UIImage(named: APPIMAGES.NoImageAvailable))
         }
-        else
-        {
+        else {
             self.topImageView.image = UIImage(named: APPIMAGES.NoImageAvailable)
         }
         self.title = viewModel.title
     }
     
-    override func viewDidLayoutSubviews() {
-        decideOrientation()
+    // As iPad Traits doesnt change, so explicitly changing it so that landscape and portrait requirement can be met.
+    // It was either this or updating constraints like in commit before this one
+    override var traitCollection: UITraitCollection {
+        if UIDevice.current.userInterfaceIdiom == .pad && UIDevice.current.orientation.isLandscape {
+            return UITraitCollection(traitsFrom:[UITraitCollection(horizontalSizeClass: .regular), UITraitCollection(verticalSizeClass: .compact)])
+        }
+        return super.traitCollection
     }
 }
 
-// Handling View Orientations
-extension DetailViewController {
-    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
-        coordinator.animate(alongsideTransition: { (UIViewControllerTransitionCoordinatorContext) -> Void in
-            self.decideOrientation()
-        }, completion: nil)
-        super.viewWillTransition(to: size, with: coordinator)
-    }
-    
-    func decideOrientation() {
-        let orient = UIApplication.shared.statusBarOrientation
-        switch orient {
-        case .portrait:
-            self.updatePortraitConstraint()
-            break
-        default:
-            self.updateLandScapeConstraint()
-            break
-        }
-    }
-    
-    func updateLandScapeConstraint() {
-        let viewFrame = self.backgroundView.frame
-        self.heightOfImage.constant = viewFrame.height
-        self.widthOfImage.constant = viewFrame.width * 0.3
-        self.heightOfLabel.constant = viewFrame.height
-        self.widthOfLabel.constant = viewFrame.width * 0.7
-    }
-    
-    func updatePortraitConstraint() {
-        let viewFrame = self.backgroundView.frame
-        self.heightOfImage.constant = viewFrame.height * 0.3
-        self.widthOfImage.constant = viewFrame.width
-        self.heightOfLabel.constant = viewFrame.height * 0.7
-        self.widthOfLabel.constant = viewFrame.width
-    }
-}
