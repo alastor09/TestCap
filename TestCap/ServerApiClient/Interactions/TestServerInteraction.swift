ffae8cf641c79d2aa3dfaf221b9c57045f438bae
Soan Saini <soan09@yahoo.com>
2018-04-15 22:46:51 +1000

    - Throwing in Dispatch Queue to parse local json files as it was sync

--- TestCap/ServerApiClient/Interactions/TestServerInteraction.swift
+++ TestCap/ServerApiClient/Interactions/TestServerInteraction.swift
@@ -11,26 +11,38 @@ import Foundation
 // Default Implementation for Testing Local Json fetch Feed
 extension ServerInteraction {
     public func fetchFeed( completionHandler: @escaping (Result<T>) -> Void) {
-        if let path = Bundle.main.path(forResource: APPURL.FactsLocalFileName, ofType: "json") {
-            do {
-                let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
-                let jsonResult = try JSONSerialization.jsonObject(with: data, options: .mutableLeaves)
-                
-                guard let jsonData = jsonResult as? [String: Any] else {
-                    completionHandler(Result.error(error: NSError(domain:"JSON Not Proper", code:1101, userInfo:nil)))
-                    return
+        
+        DispatchQueue.global(qos: .background).async {
+            if let path = Bundle.main.path(forResource: APPURL.FactsLocalFileName, ofType: "json") {
+                do {
+                    let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
+                    let jsonResult = try JSONSerialization.jsonObject(with: data, options: .mutableLeaves)
+                    
+                    guard let jsonData = jsonResult as? [String: Any] else {
+                        DispatchQueue.main.async {
+                            completionHandler(Result.error(error: NSError(domain:"JSON Not Proper", code:1101, userInfo:nil)))
+                        }
+                        return
+                    }
+                    
+                    if !self.canParse(jsonData: jsonData) {
+                        DispatchQueue.main.async {
+                            completionHandler(Result.error(error: NSError(domain:"Json not in Right Format", code:1103, userInfo:nil)))
+                        }
+                        return
+                    }
+                    
+                    let feedReceived = self.parseFeed(jsonData: jsonData)
+                    
+                    DispatchQueue.main.async {
+                        completionHandler(Result.response(feedReceived))
+                    }
+                    
+                } catch {
+                    DispatchQueue.main.async {
+                        completionHandler(Result.error(error: NSError(domain:"Unable to Parse JSON", code:1104, userInfo:nil)))
+                    }
                 }
-                
-                if !self.canParse(jsonData: jsonData) {
-                    completionHandler(Result.error(error: NSError(domain:"Json not in Right Format", code:1103, userInfo:nil)))
-                    return
-                }
-                
-                let feedReceived = self.parseFeed(jsonData: jsonData)
-                completionHandler(Result.response(feedReceived))
-                
-            } catch {
-                completionHandler(Result.error(error: NSError(domain:"Unable to Parse JSON", code:1104, userInfo:nil)))
             }
         }
     }
